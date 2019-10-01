cf0d58c4b46f4ed5eac0d6c2a7b57adefb11f0be
Sudhanya Mukhopadhyay <sudhanya.mukhopadhyay@gmail.com>
2019-10-01 11:39:04 -0700

    initial commit added SimpleClassMappinMAvenProject

---
+++ SimpleClassMappingMaven/src/main/java/com/zensar/hibernate/hibernateMain.java
@@ -0,0 +1,30 @@
+package com.zensar.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.zensar.hibernate.entities.Product;
+
+public class hibernateMain 
+{
+    public static void main( String[] args )
+    {
+    
+    		Configuration cfg = new Configuration().configure();
+    		SessionFactory sf = cfg.buildSessionFactory();
+    		Session s = sf.openSession();
+    		Transaction t = s.beginTransaction();
+    		
+    		Product product = new Product();
+    		product.setProductId(1010);
+    		product.setName("Realme 3 pro");
+    		product.setBrand("Realme");
+    		product.setPrice(13999);
+    		
+    		s.save(product);
+    		t.commit();
+    		s.close();
+    }
+}
